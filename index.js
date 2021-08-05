2cf09b3da585d442e6ebcde539aaf0e63a0440c5
Erisvaldo <erisvaldojunior@gmail.com>
2021-08-05 05:35:52 -0300

    UniversalCanvas component + typescript support

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
-import GCanvasView from './components/GCanvasComponent';
-import GImage from './packages/gcanvas/src/env/image';
+import GCanvasView from "./components/GCanvasComponent";
+import GImage from "./packages/gcanvas/src/env/image";
+import UniversalCanvas from "./components/UniversalCanvas";
 
 module.exports = {
   GCanvasView,
   GImage,
-}
+  UniversalCanvas,
+};
